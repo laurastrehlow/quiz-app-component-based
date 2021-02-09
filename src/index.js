b97c5d14549badcaf55bd9f3931d0d5ec4d34281
Laura Strehlow <laurastrehlow09@aol.com>
2021-02-09 16:10:08 +0100

    Create a new card

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
-import Button from './components/Button'
-import Header from './components/Header'
 import Card from './components/Card'
+import CreateForm from './components/CreateForm'
+import Header from './components/Header'
+import Navigation from './components/Navigation/Navigation'
+import createElement from './lib/createElement'
 
-const header = Header('Quiz App', 'May the best win!')
-document.body.append(header)
+const cards = []
 
-const card = Card('What does git log do', 'Show the history of commits')
-document.body.append(card)
+const { el: headerEl } = Header('Quiz App', 'May the best win!')
 
-const button = Button('Click me')
-document.body.append(button)
+const navigation = Navigation(onNavigate)
+
+const homePage = createElement('main', { className: 'HomePage', hidden: false })
+
+const form = CreateForm(onSubmit)
+
+const createPage = createElement(
+  'main',
+  { className: 'CreatePage', hidden: true },
+  form
+)
+
+const grid = createElement(
+  'div',
+  { className: 'appGrid' },
+  headerEl,
+  homePage,
+  createPage,
+  navigation
+)
+
+document.body.append(grid)
+
+function onSubmit(question, answer) {
+  cards.push({ question, answer })
+  renderCards()
+}
+
+function renderCards() {
+  const cardElements = cards.map(({ question, answer }) =>
+    Card(question, answer)
+  )
+  homePage.innerHTML = ''
+  homePage.append(...cardElements)
+}
+
+function onNavigate(text) {
+  if (text === 'Home') {
+    homePage.hidden = false
+    createPage.hidden = true
+  }
+
+  if (text === 'Create') {
+    homePage.hidden = true
+    createPage.hidden = false
+  }
+}
